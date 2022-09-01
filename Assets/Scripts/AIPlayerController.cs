e1d2ef493b08c30305607741f9c67cd174507753
Taffaz <prog@taffaz.com>
2022-09-01 14:07:53 +0100

    Tidied Up AI Code

--- Assets/Scripts/AIPlayerController.cs
+++ Assets/Scripts/AIPlayerController.cs
@@ -6,21 +6,34 @@ using Random = UnityEngine.Random;
 
 public class AIPlayerController : MonoBehaviour
 {
-    private const float AIUpdateTime = 0.5f;
-    private Vector3 movementPointer;
-    private Skater selectedSkater;
-    private TeamMember selectedTeamMember;
-    private Goaltender goaltender;
 
-    private Skater opponentSkater;
-    private TeamMember opponentTeamMember;
-    private Goaltender opponentGoaltender;
+    #region AIParameters
+
+    public float AIUpdateTime = 0.5f;
+    public float AIShotDistance = 10f;
+    public float AIBodyCheckDistance = 1f;
+
+    #endregion
+
+    private Vector3 movementPointer;
+    public Skater selectedSkater;
+    public TeamMember selectedTeamMember;
+    public Goaltender goaltender;
+
+    public Skater opponentSkater;
+    public TeamMember opponentTeamMember;
+    public Goaltender opponentGoaltender;
 
     private Transform puckTransform;
+    public Transform homeGoalOrigin, awayGoalOrigin;
 
-    IEnumerator aiUpdateTime;
+    AbstractAIState currentState;
 
+    Dictionary<string, AbstractAIState> stateDictionary;
     private bool isDoingSomething = false;
+
+    public Transform PuckTransform { get => puckTransform; }
+
     // If one team doesn't have a player, this object is assigned to that team.
     // Whereas the 'PlayerController' prefab uses player input to trigger functions resulting in behaviour
     // This object will use it's own logic to emulate those same input values.
@@ -33,6 +46,9 @@ public class AIPlayerController : MonoBehaviour
         opponentSkater = GameObject.FindWithTag("awaySkater").GetComponent<Skater>();
         opponentGoaltender = GameObject.FindWithTag("awayGoaltender").GetComponent<Goaltender>();
 
+
+        gameObject.name = "AI Controller Home";
+
         InitializeTeamObjects();
     }
     public void SetToAwayTeam()
@@ -41,12 +57,17 @@ public class AIPlayerController : MonoBehaviour
         goaltender = GameObject.FindWithTag("awayGoaltender").GetComponent<Goaltender>();
         opponentSkater = GameObject.FindWithTag("homeSkater").GetComponent<Skater>();
         opponentGoaltender = GameObject.FindWithTag("homeGoaltender").GetComponent<Goaltender>();
+
+        gameObject.name = "AI Controller Away";
+
         InitializeTeamObjects();
     }
     private void InitializeTeamObjects()
     {
         selectedTeamMember = selectedSkater.gameObject.GetComponent<TeamMember>();
         opponentTeamMember = opponentSkater.gameObject.GetComponent<TeamMember>();
+        homeGoalOrigin = GameObject.Find("HomeGoalOrigin").transform;
+        awayGoalOrigin = GameObject.Find("AwayGoalOrigin").transform;
         goaltender.FindMyNet();
     }
     private void HandleMovement()
@@ -54,103 +75,42 @@ public class AIPlayerController : MonoBehaviour
         // move in the given direction
         selectedSkater.SetPointers(movementPointer);
     }
-    private IEnumerator ChaseDownPuck()
-    {
-
-        isDoingSomething = true;
-        Debug.Log($"AI pursuing puck");
-        while (!selectedTeamMember.hasPosession)
-        {
-
-            bool closeToGoal = !selectedTeamMember.hasPosession && Vector3.Distance(opponentSkater.transform.position, goaltender.transform.position) < 10f;
-
-            Vector3 targetPoint = GetMovementPointer(puckTransform.position, selectedSkater.transform);
-            if (closeToGoal)
-            {
-                if (opponentTeamMember.hasPosession)
-                {
-                    print("A");
-                    targetPoint = GetGoalTenderMovementPointer(puckTransform.position);
-                }
-                else
-                {
-                    RaycastHit hitInfo;
-                    print("B");
-                    if (Physics.Raycast(puckTransform.position, puckTransform.GetComponent<Rigidbody>().velocity, out hitInfo, 10f))
-                    {
-                        print("C");
-                        if (hitInfo.collider.tag == "homeNet" || hitInfo.collider.tag == "awayNet")
-                        {
-                            print("D");
-                            targetPoint = GetGoalTenderMovementPointer(puckTransform.position);
-                        }
-                    }
-                }
-
-            }
-
-            selectedSkater.SetPointers(targetPoint);
-            goaltender.SetPointers(targetPoint);
-
-
-            if (Vector3.Distance(opponentSkater.transform.position, selectedSkater.transform.position) < 1.5f)
-            {
-                selectedSkater.WindUpBodyCheck();
-                yield return new WaitForSeconds(0.1f);
-                selectedSkater.DeliverBodyCheck();
-            }
-
-            yield return new WaitForSeconds(AIUpdateTime); // commit to doing this for a some time
-        }
-        movementPointer = Vector3.zero;
-        isDoingSomething = false;
-    }
-
-    private IEnumerator AttackGoal()
-    {
-        isDoingSomething = true;
-        Debug.Log($"AI attacking goal");
-        while (selectedTeamMember.hasPosession && Vector3.Distance(opponentGoaltender.myNet.transform.position, selectedSkater.transform.position) > 10f)
-        {
-            if (goaltender.teamMember.hasPosession)
-            {
-                goaltender.WindUpPass();
-                yield return null;
-                goaltender.PassPuck();
-            }
-            Vector3 targetPoint = GetMovementPointer(GetRandomAttackPointer(), selectedTeamMember.transform);
-            selectedSkater.SetPointers(targetPoint);
-            goaltender.SetPointers(targetPoint);
-            yield return new WaitForSeconds(AIUpdateTime); // commit to doing this for a some time
-
-        }
-
-        movementPointer = Vector3.zero;
-        isDoingSomething = false;
-
-        if (selectedTeamMember.hasPosession)
-        {
-            StartCoroutine(Shoot());
-        }
-
-    }
 
     private IEnumerator Shoot()
     {
-        isDoingSomething = true;
-        //Look at shot point
-        Vector3 shotTarget = GetRandomPointInOpponentGoal();
-        selectedSkater.SetPointers(GetMovementPointer(shotTarget, selectedTeamMember.transform));
-        yield return new WaitForSeconds(0.1f);
         StartCoroutine(selectedSkater.WindUpShot());
         yield return null;
         selectedSkater.ShootPuck();
-        isDoingSomething = false;
+    }
+
+    public void CommandShot()
+    {
+        StartCoroutine(Shoot());
+    }
+
+    public void CommandBodyCheck()
+    {
+        StartCoroutine(Bodycheck());
+    }
+
+    private IEnumerator Bodycheck()
+    {
+        selectedSkater.WindUpBodyCheck();
+        yield return new WaitForSeconds(0.1f);
+        selectedSkater.DeliverBodyCheck();
     }
 
     void Start()
     {
+        stateDictionary = new Dictionary<string, AbstractAIState>();
+        stateDictionary.Add("Waiting", new WaitingState(this));
+        stateDictionary.Add("Chase", new ChaseState(this));
+        stateDictionary.Add("Attacking", new AttackingState(this));
+        stateDictionary.Add("Shooting", new ShootingState(this));
+        stateDictionary.Add("GoalieDefend", new GoalieDefendState(this));
 
+        currentState = stateDictionary[WaitingState.StateName];
+        currentState.OnEnter();
     }
 
     private void OnEnable()
@@ -163,110 +123,30 @@ public class AIPlayerController : MonoBehaviour
         PuckScript.onPuckSpawned -= PuckSpawned;
     }
 
-    private void PossessionChange(object sender, bool e)
-    {
-        isDoingSomething = false;
-        StopAllCoroutines();
-    }
-
     private void PuckSpawned(object sender, EventArgs e)
     {
         puckTransform = (sender as PuckScript).transform;
+        ChangeState(ChaseState.StateName);
     }
 
     void Update()
     {
 
-        if (puckTransform == null)
+        currentState.OnUpdate();
+    }
+
+    public void ChangeState(string newState)
+    {
+        currentState.OnExit();
+        print($"{gameObject.name} changed from {currentState} to {newState}");
+        if (stateDictionary.TryGetValue(newState, out AbstractAIState state))
         {
+            currentState = state;
+            currentState.OnEnter();
             return;
         }
 
-        // read the situation, decide what to do
-        // call coroutines StartCoroutine(DoSomething())
-        // am I already doing something?
 
-        if (!isDoingSomething)
-        {
-            // do I have posession?
-            if (!selectedTeamMember.hasPosession)
-            {
-                StartCoroutine(ChaseDownPuck());
-                return;
-            }
-            // or try to score
-            if (!selectedSkater.WindingUp())
-                StartCoroutine(AttackGoal());
-        }
-
-
-        // Is there something more important I should do?
-        // cancel the current action and respond to it
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "awayNet" || other.tag == "homeNet")
-        {
-            selectedSkater.SetPointers(GetMovementPointer(GetRandomAttackPointer(), selectedTeamMember.transform));
-        }
-    }
-
-    private Vector3 GetMovementPointer(Vector3 targetPosition, Transform playerOrGoaltender)
-    {
-        Vector3 movementVector = targetPosition - playerOrGoaltender.position;
-        // zero the y axis (only moving in x, z)
-        movementVector.y = 0;
-        // normalize the vector
-        movementVector.Normalize();
-        // Set movementPointer to the normalized vector
-        return movementVector;
-    }
-
-    private Vector3 GetGoalTenderMovementPointer(Vector3 targetPosition)
-    {
-        Vector3 goalToPuck = targetPosition - goaltender.myNet.transform.position;
-        // zero the y axis (only moving in x, z)
-        goalToPuck.y = 0;
-        // normalize the vector
-        goalToPuck.Normalize();
-
-
-
-        Vector3 movementVector = goalToPuck - goaltender.transform.position;
-        // zero the y axis (only moving in x, z)
-        movementVector.y = 0;
-        // normalize the vector
-        movementVector.Normalize();
-        Debug.DrawLine(goaltender.transform.position, goaltender.transform.position + movementVector);
-        // Set movementPointer to the normalized vector
-        return movementVector;
-    }
-
-    private Vector3 GetRandomPointInOpponentGoal()
-    {
-        Bounds bounds = opponentGoaltender.myNet.GetComponent<Collider>().bounds;
-
-        return new Vector3(
-            Random.Range(bounds.min.x, bounds.max.x),
-            Random.Range(bounds.min.y, bounds.max.y),
-            Random.Range(bounds.min.z, bounds.max.z)
-        );
-    }
-
-    private Vector3 GetRandomAttackPointer()
-    {
-        return new Vector3(
-            opponentGoaltender.myNet.transform.position.x + 5,
-            0,
-            Random.Range(-7f, 7f)
-        );
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = selectedTeamMember.isHomeTeam ? Color.green : Color.red;
-        Gizmos.DrawSphere(new Vector3(selectedTeamMember.transform.position.x + movementPointer.x, movementPointer.y, selectedTeamMember.transform.position.z + movementPointer.z), 0.2f);
+        Debug.Log($"{newState}: Not found in StateDictionary");
     }
 }
---
+++ Assets/Scripts/AIStates/WaitingState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitingState : AbstractAIState
+{
+
+    public static new string StateName = "Waiting";
+
+    public WaitingState(AIPlayerController aiPlayerController) : base(aiPlayerController)
+    {
+    }
+
+    public override void OnEnter()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+
+    }
+}
